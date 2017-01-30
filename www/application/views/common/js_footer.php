4947ce4740dac8987afb00648aa8d6037a9d1ad7
André Luiz Girol <andregirol@gmail.com>
2017-01-30 17:55:40 -0200

    Prototipo - Tela de Login e common files
    Adicionando arquivos comuns a todas as views dos protótipos

---
+++ www/application/views/common/js_footer.php
@@ -0,0 +1,11 @@
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
---
+++ www/application/views/prototipo/login.php
@@ -0,0 +1,39 @@
+
+<body>
+
+    <div class="container">
+        <div class="row">
+
+            <h1>Departamento de física - FFCLRP </h1> 
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-info">
+                    <div class="panel-heading">
+                        <h3 class="panel-title"><i class="fa fa-wrench fa-fw"></i>Serviço de Atendimento Técnico</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form role="form">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Senha" name="senha" type="password" value="">
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input name="remember" type="checkbox" value="Lembre-me">Lembre-me
+                                    </label>
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+                                <a href="index.html" class="btn btn-lg btn-primary btn-block">Login</a>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
