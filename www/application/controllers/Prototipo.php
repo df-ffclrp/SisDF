3e04a944e71ce1b928d296aedd7b2141f23b90fa
André Luiz Girol <andregirol@gmail.com>
2017-01-30 17:52:16 -0200

    Prototipagem - Controller base
    Apenas um controller base para homologar os protótipos de telas

---
+++ www/application/controllers/Prototipo.php
@@ -0,0 +1,59 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Prototipo extends CI_Controller {
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     * 	- or -
+     * 		http://example.com/index.php/welcome/index
+     * 	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+    
+    
+    
+    public function index() {
+        //$this->load->view('common/header');
+//		$this->load->view('login');
+        echo "indexando";
+        //$this->load->view('common/footer');
+    }
+
+    /*
+     * Protótipo da página de login
+     */
+    public function login() {
+        
+        $this->load->view('common/header');
+        $this->load->view('prototipo/login');
+        $this->load->view('common/js_footer');
+        
+    }
+    
+    /*
+     * Protótipo do painel do gestor
+     */
+    
+    public function gestor() {
+        
+        $this->load->view('common/header');
+        $this->load->view('prototipo/dash_gestor');
+        $this->load->view('common/js_footer');
+        
+    }
+
+}
