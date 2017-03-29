bd340a8e70bbe309cda7ac32b55698445d6bcd26
André Luiz Girol <andregirol@gmail.com>
2017-03-29 15:12:02 -0300

    Login WIP- Método para logoff

--- www/application/controllers/Login.php
+++ www/application/controllers/Login.php
@@ -14,7 +14,7 @@ class Login extends MY_Controller {
 
     public function __construct() {
         parent::__construct();
-        
+
         $this->load->library('form_validation');
         $this->load->model('login_model');
     }
@@ -30,19 +30,65 @@ class Login extends MY_Controller {
             $this->load->view('login');
 
         else:
-            
+
             $num_usp = $this->input->post('num_usp');
             $senha = $this->input->post('senha');
 
             $dados_usuario = $this->login_model->check_user($num_usp, $senha);
-            
+
             // Registra Sessão
             $this->session->set_userdata($dados_usuario);
-            
+
             var_dump($dados_usuario);
+
+            $this->_check_user_rights($_SESSION['nivel_acesso']);
             var_dump($_SESSION);
 
         endif;
+    }
+
+    /*
+     * Desloga o usuário
+     */
+
+    public function sair() {
+        unset($_SESSION);
+        session_destroy();
+
+
+        echo "Fez logoff!<br>";
+        $url = base_url();
+        echo '<a href="' . $url . '"> Logar </a>';
+    }
+
+    //===================================
+    //Métodos Privados
+    //===================================
+
+    /*
+     * Checa nível de acesso do usuário e seta estado de conexão
+     * 
+     * Esse método será melhorado ao longo das versões do software
+     * 
+     */
+
+    public function _check_user_rights($user_level) {
+        switch ($user_level):
+            case 1;
+                $_SESSION['logged_in'] = 'gestor';
+                break;
+            case 2;
+                $_SESSION['logged_in'] = 'tecnico';
+                break;
+            case 3;
+                $_SESSION['logged_in'] = 'relator';
+                break;
+            case 4;
+                $_SESSION['logged_in'] = 'admin';
+                break;
+            default:
+                session_destroy();
+        endswitch;
     }
 
 }
