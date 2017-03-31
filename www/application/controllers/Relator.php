2599707dae3bc3b3aeede58f9cceb7e7ccffe789
André Luiz Girol <andregirol@gmail.com>
2017-03-31 18:49:38 -0300

    Container OS para agrupar chamados
    Criação de método privado para ajudar no agrupamento de ordens de serviço

--- www/application/controllers/Relator.php
+++ www/application/controllers/Relator.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Relator extends MY_Controller {   
+class Relator extends MY_Controller {
 
     /**
      * Controller para nível de acesso de
@@ -13,60 +13,93 @@ class Relator extends MY_Controller {
      * - Abre chamados
      * - Lista chamados abertos por ele
      */
-    
     public function __construct() {
         parent::__construct();
-        
+
         check_login('relator'); //custom
-        
+
         $this->load->helper('alert_box'); //custom
+        $this->load->model('relator_model');
     }
+
     /*
      * Painel do relator. Lista chamados
      */
-    
+
     public function index($secao = NULL) {
-        
+
         $secao_exists = $this->_check_secao($secao);
-        
+
         // Se a seção não existir, mostra todas
         if (!$secao_exists):
             $to_view['header'] = '';
             $to_view['header_icon'] = 'fa-tasks';
-            
+
             // mostra todos
-            // Carrega aqui a model
+            $os_abertas = $this->relator_model->get_chamados($_SESSION['id_usuario']);
+
+            var_dump($os_abertas);
+
+            $os_status_array = $this->relator_model->get_os_status();
             
+            $os_container = $this->_make_os_container($os_status_array);
+            
+            var_dump($os_container);
+
+
         endif;
-        
-        
-        
+
+
+
         //Dados para os menus
         $to_view['controller'] = $this->router->class;
         $to_view['secoes'] = $this->get_secoes();
-        
+
         // Monta dados na view para passar ao parser
-        $to_parser['conteudo'] = $this->load->view('chamados', $to_view , TRUE);
-        
-        $this->parser->parse('templates/principal',$to_parser);
-        
+        $to_parser['conteudo'] = $this->load->view('chamados', $to_view, TRUE);
+
+        $this->parser->parse('templates/principal', $to_parser);
     }
-    
-    
+
     /*
      * Checa se uma seção solicitada no index existe no array de configuração
      */
-    
-    private function _check_secao($id_secao){
+
+    private function _check_secao($id_secao) {
         $secoes = $this->get_secoes();
-        
-        foreach ($secoes as $secao){
-            if ($secao['id_secao'] == $id_secao){
+
+        foreach ($secoes as $secao) {
+            if ($secao['id_secao'] == $id_secao) {
                 return true;
             }
         }
-        
+
         return false;
     }
+
+    /*
+     * Agrupa as Ordem de serviço por status
+     */
+
+    private function _group_by_status($result_array) {
         
+    }
+
+    /*
+     * Cria um container utilizando os alias cadastrados no banco
+     * 
+     * Serve para agrupar as Ordens de Serviço e mostrá-las na View
+     */
+
+    private function _make_os_container($array_status) {
+
+        $os_container = [];
+        foreach ($array_status as $status => $propriedade) {
+
+            $os_container[$propriedade['alias']] = [];
+        }
+        
+        return $os_container;
+    }
+
 }
---
+++ www/application/models/Relator_model.php
@@ -0,0 +1,45 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Model para a página inicial de listagem de chamados
+ *
+ * @author André Luiz Girol - Departamento de Física - FFCLRP
+ */
+class Relator_model extends CI_Model {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    /*
+     * Busca todos os chamados abertos por um
+     * determinado usuário
+     */
+    
+    public function get_chamados($id_usuario){
+        $this->db->select('id_os, resumo, data_abertura, id_status, nome_status as os_status');
+        $this->db->from('ordem_servico');
+        $this->db->join('os_status', 'id_status = id_status_fk');
+
+        $this->db->where('id_usuario_fk', $id_usuario);
+        
+        $result = $this->db->get();
+        
+        return $result->result_array();
+        
+    }
+    
+    /*
+     * Busca todos os status
+     * cadastrados no sistema
+     */
+    
+    public function get_os_status(){
+        $result = $this->db->get('os_status');
+        
+        return $result->result_array();
+    }
+}
