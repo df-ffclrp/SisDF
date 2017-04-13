011ad98faff99bd57c2aa340cf9ebdbee6d55bc3
André Luiz Girol <andregirol@gmail.com>
2017-04-13 18:58:30 -0300

    Relator - ver OS por status - WIP

--- www/application/controllers/Relator.php
+++ www/application/controllers/Relator.php
@@ -27,7 +27,6 @@ class Relator extends MY_Controller {
 
         $this->_set_ui_data();
 
-        //var_dump($this->to_view);
     }
 
     /*
@@ -53,24 +52,13 @@ class Relator extends MY_Controller {
         $this->parser->parse('templates/principal', $this->to_parser);
     }
 
-    /*
-     * Busca um único chamado aberto pelo usuário
-     * 
-     * link para o botão ver_chamado
-     */
-    
-    
-    public function ver_os($id_os) {
-
-
-    }
-
 
     /*
-    Mostra ordens de serviços agrupadas por status
+        Mostra ordens de serviço agrupadas por status
     */
 
     public function os_status($id_status_os = NULL){
+
         $status_exists = $this->_check_status($id_status_os);
 
         // Se status não existe, redireciona para a página inicial
@@ -78,21 +66,14 @@ class Relator extends MY_Controller {
             $this->redirection($this->router->class);
         }
         
-        $to_view['header'] = 'Todos';
-        $to_view['header_icon'] = 'fa-tasks';
-        
-        //Dados para os menus
-        $to_view['controller'] = $this->router->class;
-        $to_view['secoes'] = $this->get_secoes();
-
         $lista_os = $this->relator_model->get_os_by_user($_SESSION['id_usuario'], $id_status_os);
 
-        $to_view['lista_os'] = $lista_os;
+        $this->to_view['lista_os'] = $lista_os;
 
     // Monta dados na view para passar ao parser
-        $to_parser['conteudo'] = $this->load->view('chamados', $to_view, TRUE);
+        $this->to_parser['conteudo'] = $this->load->view('chamados', $this->to_view, TRUE);
 
-        $this->parser->parse('templates/principal', $to_parser);
+        $this->parser->parse('templates/principal', $this->to_parser);
     }
 
 
@@ -118,7 +99,7 @@ class Relator extends MY_Controller {
     */
 
     private function _check_status($id_status_os){
-        $os_status_array = $this->relator_model->get_os_status();
+        $os_status_array = $this->get_os_status();
 
         // var_dump($os_status_array);
         foreach ($os_status_array as $status) {
@@ -145,54 +126,11 @@ class Relator extends MY_Controller {
         $this->to_view['header_icon'] = 'fa-tasks';
         $this->to_view['secoes'] = $this->get_secoes();
         $this->to_view['controller'] = $this->router->class;
+
     }
 
 
-    // ======== Serão substituídos por AJAX ==============
-
-    /*
-     * Agrupa as Ordem de serviço por status
-     * colocando no container gerado no outro método
-     * 
-     */
-
-    private function _group_by_status($result_array,$container) {
-
-        foreach ($result_array as $indice => $os){
-
-            array_push($container[$os['status_alias']], $os);
-            unset($result_array[$indice]); //Limpando memória
-
-        }        
-        
-        return $container;
-    }
-
-    /*
-     * Cria um container utilizando os alias cadastrados no banco
-     * 
-     * Serve para agrupar as Ordens de Serviço e mostrá-las na View
-     * 
-     * Formato de saída:
-     * 
-     * $os_container = array(
-     *      status1 => []
-     *      status2 => []
-     *      status3 => []
-     * );
-     */
-
-    private function _make_os_container($array_status) {
-
-        $os_container = [];
-        foreach ($array_status as $status) {
-
-            $os_container[$status['alias']] = [];
-            
-        }
-        
-        return $os_container;
-    }
+    
 
 
 }
