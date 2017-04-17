a7980d275fc2d8769691a3e13606f423e912458b
André Luiz Girol <andregirol@gmail.com>
2017-04-17 18:10:48 -0300

    Rascunho - Página de informação da OS

---
+++ rascunhos/info_chamado.php
@@ -0,0 +1,90 @@
+
+<div class="row">
+    <div class="col-lg-12">
+        <h2 class="page-header">
+            <i class="fa fa-file-text-o fa-fw"></i> 
+            OS Nº 487 - Oficina Mecânica
+        </h2>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+
+<!--Form-->
+<div class="row">
+
+
+    <div class="col-lg-8">
+
+        <!--Dados do Solicitante-->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <strong> <i class="fa fa-address-card fa-fw"></i> Dados do Solicitante</strong>
+            </div>
+
+            <div class="panel-body">
+                <p> <strong>Nome:</strong> Ademir Almeida </p>
+                <p> <strong>Número USP:</strong> 12365488 </p>
+                <p> <strong>Ramal:</strong> 0099 </p>
+                <p> <strong>Email:</strong> almeida@usp.br </p>
+            </div>
+        </div>
+
+        <!--Detalhes do Pedido-->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <strong> <i class="fa fa-list fa-fw"></i> Detalhes do Pedido</strong>
+            </div>
+
+            <div class="panel-body">
+                <p> 
+                    <strong>Data de Abertura:</strong> 2017-03-22 12:12:11 <br>
+                    <strong>Finalidade:</strong> Projetos didáticos <br>
+                    <strong>Local:</strong> Sala 42 - Laboratório Biomag <br>
+                    <strong>Responsável:</strong>  Prof. Belmiro Rosa
+
+                </p>
+                <p> <strong>Resumo:</strong> Troca de Lâmpada </p>
+
+                <p> <strong>Descrição do Pedido:</strong> </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                    irure dolor in reprehenderit in voluptate velit esse cillum 
+                </p>
+
+            </div>
+        </div>
+
+        <!--Dados do Material-->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <strong> <i class="fa fa-cubes fa-fw"></i> Dados do Material</strong>
+            </div>
+
+            <div class="panel-body">
+                <p> <strong>Descrição do Material:</strong> </p>
+                <p> <strong>Fornecimento: </strong> Solicitante</p> 
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                    irure dolor in reprehenderit in voluptate velit esse cillum 
+                </p>
+            </div>
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">Voltar</button>
+        <button class="btn btn-default pull-right">
+            <i class="fa fa-print fa-fw"></i> 
+            Imprimir
+        </button>
+
+
+
+    </div>
+</div>
+<!-- END Form-->
