8f458fa5e0881b65f0ddf3af01db97f2af1f53cc
André Luiz Girol <andregirol@gmail.com>
2017-09-20 18:04:17 -0300

    Add custom SisDF CSS

--- www/application/views/common/header.php
+++ www/application/views/common/header.php
@@ -25,6 +25,9 @@
 
         <!-- Custom Fonts -->
         <link href="<?= base_url() ?>assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        
+        <!-- CSS SisDF -->
+        <link href="<?= base_url() ?>assets/custom/sisdf.css" rel="stylesheet" type="text/css">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
