b6664b982d13cbda3f830a170cb6d9910b914a8b
André Luiz Girol <andregirol@gmail.com>
2017-10-24 15:17:11 -0200

    Sandbox - Adicionando password reset
    Adicionando reset de senha para base de desenvolvimento para facilitar o debugging

--- SQL/sample_data/make_sandbox.sh
+++ SQL/sample_data/make_sandbox.sh
@@ -2,7 +2,7 @@
 
 source config.sh
 
-echo "Realizando dump de: $remote_host de $remote_database";
+echo "Realizando dump de: $remote_host de $remote_db";
 
 mysqldump -h$remote_host --user $remote_user -p$remote_pass $remote_db > sisdf_replicate.sql
 
@@ -10,5 +10,6 @@ echo "Reimportando dados localmente"
 
 mysql -h$dev_host --user $dbuser -p$dbpass $dev_database < sisdf_replicate.sql
 
-# future implement
-#mysql -u root -pmy_password -D DATABASENAME -e "UPDATE `database` SET `field1` = '1' WHERE `id` = 1111;" > output.txt
+# Sets all passwords to 123
+echo "Resetando senhas para testes"
+mysql -u$dbuser -p$dbpass -D $dev_database < reset_passwords.sql
