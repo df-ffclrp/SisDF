8f8c1920e2f8d7d00057d5c5d1ef8971a48d09c6
André Luiz Girol <andregirol@gmail.com>
2019-02-21 14:31:32 -0300

    Refactor: Auth Library - método in_role()
    - Documentando com phpDoc
    - Formatando para PSR-2 (usando VSCode)
    - Refatorando método in_role com documentação completa
    - Utilização da Model para recuperar dados

--- www/application/libraries/Auth.php
+++ www/application/libraries/Auth.php
@@ -1,43 +1,44 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 Biblioteca Padrão de Autenticação
 
 @author André Luiz Girol / FFCLRP - USP Ribeirão Preto
 
-*/
+ */
 
-class Auth {
+class Auth
+{
 
     private $CI;
 
     private $roles = [];
 
-    public function __construct() {
+    public function __construct()
+    {
 
         // Pega a instância do CI - super-object
-        $this->CI = & get_instance();
+        $this->CI = &get_instance();
         $this->CI->load->model('auth_model');
 
         $this->roles = $this->CI->auth_model->get_roles();
 
     }
     /**
-    *  Checa o usuário logado no sistema
-    */
-    public function check_login() {
+     *  Checa o usuário logado no sistema
+     */
+    public function check_login()
+    {
         $this->CI->load->helper('url');
 
-        if (!isset($_SESSION['nivel_acesso'])):
-
+        if (!isset($_SESSION['num_usp'])) {
             session_destroy();
             $uri = base_url() . 'login';
             redirect($uri, 'location', 301);
-
             exit();
-        endif;
+        }
     }
 
 
@@ -45,48 +46,48 @@ class Auth {
         Verifica se o usuário é dono do recurso a ser visualizado
 
         Por enquanto apenas verifica donos de OS
-    */
+     */
 
-    public function is_owner($os_metadata){
+    public function is_owner($os_metadata)
+    {
 
         $id_usuario = $_SESSION['id_usuario'];
 
-        if($os_metadata['id_relator'] == $id_usuario){
-            return TRUE;
-        } else {
-            return FALSE;
+        if ($os_metadata['id_relator'] == $id_usuario) {
+            return true;
         }
+        return false;
 
     }
     /*
     Checa se o usuário está em determinada seção
     de atendimento
-    */
+     */
 
-    public function in_secao($secao = FALSE){
+    public function in_secao($secao = false)
+    {
 
-        if($secao === $_SESSION['membro_secao']){
-            return TRUE;
+        if ($secao === $_SESSION['membro_secao']) {
+            return true;
         } else {
-            return FALSE;
+            return false;
         }
     }
 
 
-    /*
-        Checa quais roles estão autorzadas a ver aquele Recurso
+    /**
+     * Checa se um usuário pertence a determinada role
+     * A model já trata o tipo de retorno true / false
+     * 
+     * @param string $role - Cadastrada no banco manualmente na tabela Roles.
+     * @return boolean
+     */
 
-        Não checa se o usuário é dono
-
-        @param $roles = string
-    */
-
-    public function in_role($role = FALSE){
-        if($role === $_SESSION['nivel_acesso']){
-            return TRUE;
-        } else {
-            return FALSE;
-        }
+    public function in_role($role = false)
+    {
+        $id_logged_user = $_SESSION['id_usuario'];
+        $autorizado = $this->CI->auth_model->check_user_role($id_logged_user, $role);
+        return $autorizado;
 
     }
 
@@ -94,13 +95,13 @@ class Auth {
         Checa se um usuário é gestor da unidade
 
         Nível Chuck Norris
-    */
-    public function is_gestor_unidade(){
-        if($_SESSION['nivel_acesso'] === 'gestor_unidade'){
-            return TRUE;
-        } else {
-            return FALSE;
+     */
+    public function is_gestor_unidade()
+    {
+        if ($_SESSION['nivel_acesso'] === 'gestor_unidade') {
+            return true;
         }
+        return false;
     }
 
 }
--- www/application/models/Auth_model.php
+++ www/application/models/Auth_model.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * Model para a biblioteca de Autenticação
@@ -8,14 +8,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author André Luiz Girol - Departamento de Física - FFCLRP
  */
 
-class Auth_model extends CI_Model {
+class Auth_model extends CI_Model
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         $this->load->database();
     }
 
-    public function get_roles(){
+    public function get_roles()
+    {
         $this->db->select('nome');
         $query = $this->db->get('role');
 
@@ -25,6 +28,30 @@ class Auth_model extends CI_Model {
         return $roles;
     }
 
+    /**
+     * Checa se determinado usuário está em uma role
+     * @param int $id_usuario 
+     * @param string $user_role 
+     */
+
+    public function check_user_role($id_usuario, $role_to_check)
+    {
+        $this->db->select('id_role, nome');
+        $this->db->from('role');
+    
+        // Que nível de acesso?
+        $this->db->join('user_role', "role_id = id_role");
+        $this->db->where('role.nome', $role_to_check);
+    
+        $this->db->where('usuario_id', $id_usuario);
+        $result = $this->db->get();
+
+        if($result->num_rows() == 1){
+            return true;
+        };
+        
+        return false;
+    }
 
 
 }
